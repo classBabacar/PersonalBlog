6725584d46f1253562e49be18961ff49aa38903f
Babacar Ndoye <babacarndoye13@gmail.com>
2020-05-29 19:53:56 -0400

    Homepage components changed

---
+++ frontend/src/app/homepage/homepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './homepage.html',
+  styleUrls: ['./homepage.scss']
+})
+export class LandingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
