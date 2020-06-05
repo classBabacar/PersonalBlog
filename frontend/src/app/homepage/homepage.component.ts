594bd36f8cf9723474743bead242b879e6e06aa9
Babacar Ndoye <babacarndoye13@gmail.com>
2020-06-05 18:41:49 -0400

    Homepage components added

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Services } from './../services/services'
 
 @Component({
   selector: 'app-landing-page',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.scss']
 })
 export class LandingPageComponent implements OnInit {
+  result: any;
+  fact: string;
+  flipped: boolean
+  constructor(private Services: Services) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    this.result = await this.Services.getRandomFact()
+    this.fact = this.result.text
+    this.flipped = false
   }
 
+  async generateNewFact() {
+    if (this.flipped == false) {
+      this.result = await this.Services.getRandomFact()
+      this.fact = this.result.text
+    }
+    this.flipped = !this.flipped
+  }
+  
 }
