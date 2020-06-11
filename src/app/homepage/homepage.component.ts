6cd145174af5fceca02ac746daa0d535f5aae5a5
Babacar Ndoye <babacarndoye13@gmail.com>
2020-06-11 02:53:40 -0400

    Homepage components changed

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -7,23 +7,32 @@ import { Services } from '../services/services'
   styleUrls: ['./homepage.scss']
 })
 export class LandingPageComponent implements OnInit {
+  connect4: boolean;
+  hangman: boolean;
+  mastermind: boolean;
   result: any;
-  fact: string;
-  flipped: boolean
+  fact: any;
+
   constructor(private Services: Services) { }
 
   async ngOnInit() {
+    this.connect4 = false
+    this.hangman = false
+    this.mastermind = false
     this.result = await this.Services.getRandomFact()
     this.fact = this.result.text
-    this.flipped = false
   }
 
-  async generateNewFact() {
-    if (this.flipped == false) {
-      this.result = await this.Services.getRandomFact()
-      this.fact = this.result.text
-    }
-    this.flipped = !this.flipped
+  async changeData(data) {
+    this.connect4 = false
+    this.hangman = false
+    this.mastermind = false
+
+    data == 'connect4' ? this.connect4 = true : data == 'hangman' ? this.hangman = true : data == 'mastermind' ? this.mastermind = true : '';
   }
   
+  async generateFact() {
+      this.result = await this.Services.getRandomFact()
+      this.fact = this.result.text
+  }
 }
